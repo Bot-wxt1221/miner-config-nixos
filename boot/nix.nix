3918acced1ba9788f76c523990be30a5853e1e16
wxt <3264117476@qq.com>
2024-09-06 10:32:58 +0800

    Upd

--- boot/nix.nix
+++ boot/nix.nix
@@ -15,6 +15,7 @@
     "https://wxt1221.cachix.org"
     "https://cuda-maintainers.cachix.org"
     "https://luogu-judge.cachix.org"
+    "https://hyprland.cachix.org"
   ];
   nix.settings.trusted-users = [
     "root"
@@ -25,6 +26,7 @@
     "wxt1221.cachix.org-1:JmeZ+5reEo1i5qBc6X+2hEadoN5GFqTOQcwaGD5PEwY="
     "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     "luogu-judge.cachix.org-1:vBoiA0n//KfYZ5Xp5HhcujXcdT7dK3+rEK3U4zw66TQ="
+    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
   ];
   nix.settings.experimental-features = [
     "nix-command"
