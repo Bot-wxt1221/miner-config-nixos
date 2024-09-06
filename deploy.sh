334a6bce45c72a6f5b1376ec076474c1ac30b72f
wxt <3264117476@qq.com>
2024-09-06 10:34:48 +0800

    Upd

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "Upd"
 git push origin master
-nixos-rebuild switch --flake .#nixos-1 --target-host root@192.168.42.225 --log-format internal-json -v |& nom --json
+nixos-rebuild switch --flake .#nixos-1 --target-host root@192.168.42.225 --build-host root@192.168.42.225  --log-format internal-json -v |& nom --json
