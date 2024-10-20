1cf426b36fd142283e5ff195a21ffa3ff9b2d3e3
wxt <3264117476@qq.com>
2024-10-20 18:29:23 +0800

    Upd

--- boot/openssh.nix
+++ boot/openssh.nix
@@ -11,7 +11,7 @@
   services.openssh.settings.PermitRootLogin = "yes";
   users.users."root".openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPfk1DLJF9a0qbrNYTGTzY59oPAO/+vU4GWA8/6U84U3 zigao"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB9OCkgPcdNq/0S7e4UtyrxYfYCoQuS16aAsQqCUd6xp wxt@wxt-school-vmware"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFr5fYeSQEdiLX1T3I4u39f5KmHuCf3tJNA2C1d0DNgv wxt@wxt-school-vmware"
   ];
   services.openssh.enable = true;
 }
